8d3b84597c3d36a0dfac91433ec0bd8d27849bc8
BernardoAbner <bernardoabnerwsp@gmail.com>
2024-10-09 19:25:02 -0300

    commit realizado para atualização do andamento do trabalho. Foi feito até o momento a criação das tabelas e a conexão do python ao mysql.  @howardroatti se puder, revise por favor.

---
+++ Trabalho_BD_Hotel/script.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    
+    connection = mysql.connector.connect(
+        host='localhost', 
+        database='trabalhoBD_hotel',  
+        user='root',  
+        password='638450'  
+    )
+    
+    if connection.is_connected():
+        db_info = connection.get_server_info()
+        print("Conectado ao servidor MySQL versão ", db_info)
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        record = cursor.fetchone()
+        print("Conectado ao banco de dados: ", record)
+
+except Error as e:
+    print("Erro ao conectar ao MySQL", e)
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("Conexão com o MySQL foi encerrada")
